d2ef377534c2b89525c55a217774bef95844c89a
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-05-30 20:00:52 +0000

    Added else if clause

--- src/script.js
+++ src/script.js
@@ -1,6 +1,30 @@
-console.log("Before consitionals");
-let random = Math.random();
+// console.log("Before consitionals");
+// let random = Math.random();
 
-if(random<0.5){
-    console.log("Your number is less than 0.5");
+// if(random<0.5){
+//     console.log("Your number is less than 0.5");
+// }
+
+// const dayOfWeek = 'Monday';
+// if(dayOfWeek == 'Monday'){
+//     console.log("I HATE MONDAYS!!")
+// } else if(dayOfWeek == 'Saturday'){
+//     console.log("I LOVE SATURDAYS!");
+// }
+
+let babyPrice = 0;
+let childPrice = 10;
+let adultPrice = 20;
+let seniorPrice = 10;
+
+const age = 2;
+
+if(age<5){
+    console.log(`Please pay $${babyPrice}`);
+} else if(age<10){
+    console.log(`Please pay $${childPrice}`);
+} else if(age<65){
+    console.log(`Please pay $${adultPrice}`);
+} else if(age>65){
+    console.log(`Please pay $${seniorPrice}`);
 }
