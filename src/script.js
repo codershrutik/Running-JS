4e045990b3c85216d11806a490ea7d30b4c5f433
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-05-30 20:07:59 +0000

    Else clause added

--- src/script.js
+++ src/script.js
@@ -1,9 +1,11 @@
-// console.log("Before consitionals");
-// let random = Math.random();
+console.log("Before conditionals");
+let random = Math.random();
 
-// if(random<0.5){
-//     console.log("Your number is less than 0.5");
-// }
+if(random<0.5){
+    console.log("Your number is less than 0.5");
+} else{
+    console.log("Your number is less than or equal t0 0.5");
+}
 
 // const dayOfWeek = 'Monday';
 // if(dayOfWeek == 'Monday'){
@@ -12,19 +14,19 @@
 //     console.log("I LOVE SATURDAYS!");
 // }
 
-let babyPrice = 0;
-let childPrice = 10;
-let adultPrice = 20;
-let seniorPrice = 10;
+// let babyPrice = 0;
+// let childPrice = 10;
+// let adultPrice = 20;
+// let seniorPrice = 10;
 
-const age = 2;
+// const age = 2;
 
-if(age<5){
-    console.log(`Please pay $${babyPrice}`);
-} else if(age<10){
-    console.log(`Please pay $${childPrice}`);
-} else if(age<65){
-    console.log(`Please pay $${adultPrice}`);
-} else if(age>65){
-    console.log(`Please pay $${seniorPrice}`);
-}
+// if(age<5){
+//     console.log(`Please pay $${babyPrice}`);
+// } else if(age<10){
+//     console.log(`Please pay $${childPrice}`);
+// } else if(age<65){
+//     console.log(`Please pay $${adultPrice}`);
+// } else if(age>65){
+//     console.log(`Please pay $${seniorPrice}`);
+// }
