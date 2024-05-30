deff2b2a98fefe479ada263707e7b5bca0921fbd
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-05-30 20:19:52 +0000

    Removed error

--- src/script.js
+++ src/script.js
@@ -38,7 +38,7 @@ if(password.length >=6){
     if(password.indexOf(' ') == -1){
         console.log('Valid Password');
     } else{
-        console.log("Password cannot contain spaces!";)
+        console.log("Password cannot contain spaces!");
     }
 } else{
     console.log("PASSWORD TOO SHORT!");
